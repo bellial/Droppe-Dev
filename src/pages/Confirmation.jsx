57d4030910561f47f2e87742c8e213d60806f260
unknown <ninja.pri@gmail.com>
2021-04-18 21:16:05 -0300

    confirmation page layout v1

--- src/pages/Confirmation.jsx
+++ src/pages/Confirmation.jsx
@@ -4,10 +4,17 @@ import { formatMoney } from "../utils/format";
 
 export default function ConfirmationPage() {
   const { approvedWishlists } = useCart();
+  const { rejectWishlist } = useCart();
   const { total, totalDiscounts } = usePricing(approvedWishlists);
 
   return (
-    <span>
+    <span style={{overflow: "auto", flex: 1,
+    padding: "64px",
+    paddingTop: "128px",
+    flexDirection: "column",
+    justifyContent: "center",
+    backgroundImage: `url('data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="4" height="4" viewBox="0 0 4 4"%3E%3Cpath fill="%239C92AC" fill-opacity="0.4" d="M1 3h1v1H1V3zm2-2h1v1H3V1z"%3E%3C/path%3E%3C/svg%3E')`
+  }}>
       <span>Order Placed</span>
       <span>Total $ {formatMoney(total)}</span>
       <span>Total Discounts $ {formatMoney(totalDiscounts)}</span>
